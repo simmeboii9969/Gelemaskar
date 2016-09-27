c6f671ecef1c312c61d62e0c0d992441b9ea8551
simmeboii9969 <simon.karlsson@elev.kungsbacka.se>
2016-09-27 13:35:50 +0200

    Hopp
    blyat

---
+++ Javascript/script.js
@@ -0,0 +1,56 @@
+var ctx, wromX, wormImage, wormVX = 0, wormY = 400, wormVY = 0, wormAY = 0;
+
+function init() {
+
+    var canvas = document.getElementById("canv");
+    ctx = canvas.getContext("2d");
+
+    wormImg = document.getElementById("wormImg");
+
+    wormX = 200;
+
+    window.setInterval(update, 20);
+}
+
+function update() {
+    
+    wormX = wormX + wormVX;
+    wormY = wormY + wormVY;
+    ctx.clearRect(0, 0, 800, 500)
+    ctx.drawImage(wormImg, wormX, wormY, 100, 80);
+
+}
+
+function keyDown(e) {
+    //console.log(e.keyCode);
+    if (e.keyCode == 65) {
+        //A
+        wormVX = -1;
+    }
+
+    if (e.keyCode == 68) {
+        //D
+        wormVX = 1;
+
+    }
+    
+    if(e.keyCode == 32){
+        //Space
+        wormVY = -2;
+        
+    }
+}
+
+function keyUp(e) {
+    //console.log(e.keyCode);
+    if (e.keyCode == 65) {
+        //A
+        wormVX = 0;
+    }
+
+    if (e.keyCode == 68) {
+        //D
+        wormVX = 0;
+
+    }
+}
